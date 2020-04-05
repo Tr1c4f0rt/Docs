e4835dc03f7db9b7173f9d766d35b9ff2b999467
Tr1c4f0rt <tr1c4f0rt@protonmail.com>
2020-04-05 12:47:24 +0200

    maquetació: Vista llistat de docs

--- pages/docs/index.js
+++ pages/docs/index.js
@@ -1,41 +1,80 @@
 import Link from 'next/link'
 
 // Comps
-import Layout from '../../components/Layout';
+import NavBar from '../../components/Layout';
 
 function Docs() {
 
-
-
-    return (<Layout>
-        <div className="tile is-ancestor">
-
-           
-
-                <div className="tile is-parent">
-                    <article className="tile is-child notification box is-primary">
-                        <p className="title">Document Zero</p>
-                        <p className="subtitle">Top tile</p>
-                        <Link href="/docs/doc-zero/">
-                        Zero
-                        </Link>
-                    </article>
+    return (     
+            <div className="hero is-fullheight">
+                <div className="hero-head">
+                    <NavBar/>
                 </div>
+                <div className="hero-body is-centered">
+                    <div className="tile is-parent">
+                        <article className="tile is-child notification card is-warning carta-doc">
+                            <div className="card-content">
+                                <p className="title">
+                                    Document Zero
+                            </p>
+                                <p className="subtitle">
+                                    Publicant-se
+                            </p>
+                            </div>
+                            <footer className="card-footer">
+                                <p className="card-footer-item">
+                                    <Link href="/docs/doc-zero/">
+                                        <a className="button is-small is-fullwidth">Llegir</a>
+                                    </Link>
+                                </p>
+                            </footer>
+                        </article>
+                    </div>
 
-           
-                <div className="tile is-parent">
-                    <article className="tile is-child notification box is-warning">
-                        <p className="title">Document Primer</p>
-                        <p className="subtitle">Bottom tile</p>
-                        <Link href="/docs/doc-primer/">
-                            Primer
-                            </Link>
-                    </article>
+
+                    <div className="tile is-parent">
+                        <article className="tile is-child notification card is-primary carta-doc">
+                            <div className="card-content">
+                                <p className="title">
+                                    Document Primer
+                            </p>
+                                <p className="subtitle">
+                                    Pendent de publicació
+                            </p>
+                            </div>
+                            <footer className="card-footer">
+                                <p className="card-footer-item">
+                                    <Link href="/docs/doc-primer/">
+                                        <a className="button is-small is-static is-fullwidth">Llegir</a>
+                                    </Link>
+                                </p>
+                            </footer>
+                        </article>
+                    </div>
+
+                    <div className="tile is-parent">
+                        <article className="tile is-child notification card is-dark carta-doc">
+
+                            <div className="card-content">
+                                <p className="title">
+                                    Document Segon
+                            </p>
+                                <p className="subtitle">
+                                    En producció
+                            </p>
+                            </div>
+                            <footer className="card-footer">
+                                <p className="card-footer-item">
+                                    <button className="button is-small is-static is-fullwidth">Llegir</button>
+                                </p>
+                            </footer>
+
+                        </article>
+                    </div>
+
                 </div>
-          
-        </div>
-
-    </Layout>)
+            </div>      
+   )
 
 
 
